21e831a3172d063be274547b7fdf92c90b9eb2f5
krisdianwidya <krisdianwidya@gmail.com>
2021-08-10 12:08:08 +0700

    create tasks data

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,28 @@ export default {
       task: [],
     };
   },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "March 1st at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Meeting at School",
+        day: "March 3rd at 1:30pm",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Food Shopping",
+        day: "March 1st at 11:00am",
+        reminder: true,
+      },
+    ];
+  },
 };
 </script>
 
