915bc3885ef54a0d4322da4117c330d33486a8e0
krisdianwidya <krisdianwidya@gmail.com>
2021-08-10 14:58:44 +0700

    create emit events, delete task and toggle reminder

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
-    <Tasks :tasks="tasks" />
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
   </div>
 </template>
 
@@ -17,8 +21,18 @@ export default {
   },
   data() {
     return {
-      task: [],
+      tasks: [],
     };
+  },
+  methods: {
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
   },
   created() {
     this.tasks = [
