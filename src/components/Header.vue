0d11ba5aef934e3c349cb5f667238ab3ad969f70
krisdianwidya <krisdianwidya@gmail.com>
2021-08-12 17:32:00 +0700

    finish routing

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,7 @@
   <header>
     <h1>{{ title }}</h1>
     <Button
+      v-show="homePg"
       @btn-click="$emit('toggle-add-task')"
       :text="showAddTask ? 'Cancel Add Task' : 'Add Task'"
       :color="showAddTask ? '' : 'green'"
@@ -20,6 +21,15 @@ export default {
   },
   components: {
     Button,
+  },
+  computed: {
+    homePg() {
+      if (this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
   },
 };
 </script>
